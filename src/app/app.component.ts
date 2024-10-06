1d2bb661724c981a219f83d311a79c7e4e73d44c
CristianLS5 <cristianlopezc4@gmail.com>
2024-10-06 18:12:31 +0200

    create intro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { LongTextComponent } from '../long-text/long-text.component';
+import { IntroComponent } from '../intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LongTextComponent],
+  imports: [RouterOutlet, HeaderComponent, LongTextComponent, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
